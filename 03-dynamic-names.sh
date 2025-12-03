93ff585a69136c773d9586b18d1752b8cc3e9a42
vasu <vasumattipelli77@gmail.com>
2025-12-03 17:44:55 +0530

    dynamic names

--- 03-dynamic-names.sh
+++ 03-dynamic-names.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-$PERSON1=abc
-$PERSON2=def
+PERSON1=$1
+PERSON2=$2
 
-echo "$PERSON1 HI RA"
-echo "$PERSON HELLO RA"
+echo "$PERSON1: hi ra $PERSON2 how are you"
+echo "$PERSON2: hi ra $PERSON1 fine how are you"
+echo "$PERSON1: fine ra what are you learning $PERSON2 "
+echo "$PERSON2: am learning devops ra $PERSON1"
