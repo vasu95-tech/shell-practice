471cc4bd5207becd58529d26e02904297c9f15e7
vasu <vasumattipelli77@gmail.com>
2025-12-06 13:52:54 +0530

    arrays new

--- 08-if-else.sh
+++ 08-if-else.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]; then
     echo " given number is greater than 10 "
-elif [ $NUMBER -eq 1o ]; then
+elif [ $NUMBER -eq 10 ]; then
     echo " given number is equal to 10 "
 else
      echo " given number is less than 10 "    
