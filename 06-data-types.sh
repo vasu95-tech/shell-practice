dc249f823a391e9acceb75fce96b0d62f980f541
vasu <vasumattipelli77@gmail.com>
2025-12-06 12:15:26 +0530

    data types

---
+++ 06-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Everything in shell is considerd as a string
+NUMBER1=100
+NUMBER2=200
+#PERSON=Vasu
+#echo "total sum is SUM=$(($NUMBER1+$NUMBER2))"
+SUM=$(($NUMBER1+$NUMBER2))
+echo "Total is = $SUM"
