560c6af5a0cc560db75fe50422bd92e9ee3b8ba2
vasu <vasumattipelli77@gmail.com>
2025-12-06 11:52:41 +0530

    date command

--- 05-date-command.sh
+++ 05-date-command.sh
@@ -2,9 +2,9 @@
 
 #DATE=$(date +s)
 START_DATE=$(date +%s)
-echo "script start time is = $DATE"
+echo "script start time is = START_DATE"
 sleep 10
 END_DATE=$(date +%s)
-echo "script end time is = $DATE"
+echo "script end time is = $END_DATE"
 TOTAL_TIME=$(($END_DATE-$START_DATE))
 echo "The total time for excuting the script is $TOTAL_TIME "
