409a09e19b6b060b6b6f83f0991f562bf1e12175
vasu <vasumattipelli77@gmail.com>
2025-12-06 13:16:54 +0530

    arrays new

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 LEADERS=("Modi" "Puthin" "trump")
-echo "all leaders are ${$LEADERS}[@]"
-echo "first leader in group is ${LEADERS}[0]
+echo "all leaders are ${LEADERS[@]}"
+echo "first leader in group is ${LEADERS[0]}
