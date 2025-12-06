2f41b613382979ea82d7e9ec11547412a08cb8dd
vasu <vasumattipelli77@gmail.com>
2025-12-06 14:07:04 +0530

    arrays new

--- 09-even-odd-cond.sh
+++ 09-even-odd-cond.sh
@@ -3,7 +3,7 @@
 echo "please enter a number"
 read NUMBER
 
-if [ $NUMBER%2 -eq 0 ]; then
+if [ $NUMBER % 2 -eq 0 ]; then
     echo " given number is even number "
 else
     echo  " given number is odd number "
